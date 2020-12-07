e8ac1065454c0d8c876cfe71e910b0ba59dea0bd
Luciano Larrieu <luciano.larrieu@gmail.com>
2020-12-07 18:56:38 +0100

    Implement backend inventory service

--- BackEndInventoryService/Service/BackEndService.cs
+++ BackEndInventoryService/Service/BackEndService.cs
@@ -11,28 +11,71 @@ namespace BackEndInventoryService.Service
     {
         private IValidator<Reservation> ReservationValidator = new ReservationValidator();
 
+        public Dictionary<string, Inventory> Products = new Dictionary<string, Inventory>();
+
+        public List<Reservation> AvailableReservations = new List<Reservation>();
+        public List<Reservation> PendingReservations = new List<Reservation>();
+
         public Reservation CreateReservation(List<OrderLine> orders)
         {
             var reservation = new Reservation(DateTime.Now, orders);
 
             ReservationValidator.ValidateAndThrow(reservation);
 
+            foreach (OrderLine order in orders)
+            {
+                if (Products.TryGetValue(order.ProductId, out Inventory inventory))
+                {
+                    reservation.IsAvailable &= Products[order.ProductId].Availability && inventory.Quantity >= order.Quantity;
+
+                    if (reservation.IsAvailable)
+                    {
+                        SetInventory(order.ProductId, inventory.Quantity - order.Quantity);
+                    }
+                    else
+                    {
+                        Products[order.ProductId].Availability = false;
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException($"ProductId [{order.ProductId}] does not exist");
+                }
+            }
+
+            AddReservation(reservation);
+
             return reservation;
         }
 
         public List<Inventory> GetInventory(int cursor, int limit)
         {
-            throw new NotImplementedException();
+            return Products.Values.Skip(cursor).Take(limit).ToList();
         }
 
         public List<Reservation> GetReservations(int cursor, int limit)
         {
-            throw new NotImplementedException();
+            return AvailableReservations.OrderBy(r => r.ReservationId).Skip(cursor).Take(limit).ToList();
         }
 
         public void SetInventory(string productId, int quantity)
         {
-            throw new NotImplementedException();
+            Inventory inventory = Products[productId];
+
+            inventory.Quantity = quantity;
+            inventory.Availability = inventory.Quantity > 0;
+        }
+
+        private void AddReservation(Reservation reservation)
+        {
+            if (reservation.IsAvailable)
+            {
+                AvailableReservations.Add(reservation);
+            }
+            else
+            {
+                PendingReservations.Add(reservation);
+            }
         }
     }
 }
